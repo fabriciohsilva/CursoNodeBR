8c65c8f268dd854dc49bf78e07fe0e27b272da17
Fabrício Henrique <fabricioh.silva@live.com>
2019-01-22 19:47:27 -0200

    Módulo 5
    Exercícios do módulo 1 ao 5

---
+++ 04-lists/for-forin/3.filter.js
@@ -0,0 +1,41 @@
+const {obterPessoas} = require('./service');
+
+Array.prototype.meuFilter = function (callback) {
+    
+    const lista = [];
+    
+    for(index in this) {
+        const item = this[index];
+        const result = callback(item,index, this)
+
+        if (!result) continue;
+
+        lista.push(item);
+    }
+    return lista;
+}
+
+async function main() {
+    try {
+        const {results} = await obterPessoas('a'); 
+
+        // const familiaLars = results.filter(function (item) {
+        //     const result = item.name.toLowerCase().indexOf('lars') !== -1;
+        //     return result;
+        // });
+
+        const familiaLars = results.meuFilter((item, index, lista) => {
+            console.log(`index: ${index} de `, lista.length);
+            return item.name.toLowerCase().indexOf('lars') !== -1;
+        });
+        
+        
+        const names = familiaLars.map(pessoa =>  pessoa.name );
+
+        console.log('names: ', names);
+    } catch (error) {
+        console.error('Erro na execução: ', error);
+    }
+}
+
+main();
---
+++ 07-multi-db/src/db/strategies/postgres.js
@@ -0,0 +1,15 @@
+const ICrud = require('./interfaces/interfaceCrud');
+
+class Postgres extends ICrud {
+
+  constructor(){
+    super();
+  }
+
+  create(item){
+    console.log("O item foi gravado em Postgres");
+  }
+
+}
+
+module.exports = Postgres;
